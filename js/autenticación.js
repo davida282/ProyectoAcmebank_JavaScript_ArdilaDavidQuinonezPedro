506e2f9837335b9d2a36e96a937944779e0a3370
davida282 <davidardila1228@gmail.com>
2025-06-13 03:13:23 -0500

    feat: :sparkles: Se añade la página de Registrar cuenta y se ajusta el Style de este.

--- js/autenticación.js
+++ js/autenticación.js
@@ -13,20 +13,3 @@ function seleccionarOpcion(event) {
     // Oculta las opciones y cierra el menú
     document.querySelector('.contenedor').classList.remove('activo');
 }
-
-// Tu configuración personalizada de Firebase
-const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  databaseURL: "https://tu-proyecto.firebaseio.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcd1234"
-};
-
-// Inicializar Firebase
-const app = firebase.initializeApp(firebaseConfig);
-
-// Inicializar la base de datos
-const db = firebase.database();
