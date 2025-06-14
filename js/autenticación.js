50a7abd36991d8f30347c4ca709179324791849e
davida282 <davidardila1228@gmail.com>
2025-06-14 13:04:41 -0500

    feat: :sparkles: Se terminan de pulir detalles en el apartado de Registro en el css y Script.

--- js/autenticación.js
+++ js/autenticación.js
@@ -1,16 +1,2 @@
-function toggleMenu() {
-    document.querySelector('.contenedor').classList.toggle('activo');
-}
 
-function seleccionarOpcion(event) {
-    event.preventDefault(); 
-
-    let textoSeleccionado = event.target.textContent; 
-
-    
-    document.querySelector('.flecha').textContent = textoSeleccionado;
-
-    
-    document.querySelector('.contenedor').classList.remove('activo');
-}
 
